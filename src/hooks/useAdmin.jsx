3eac728573097849b51861f7f004ff5449707109
coder7475 <robiulhossain7475@gmail.com>
2023-11-26 14:21:18 +0600

    feat: add useAdmin hook

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,21 @@
+import useAuth from "./useAuth";
+import useSecureAxios from "./useSecureAxios";
+import { useQuery } from "@tanstack/react-query";
+
+const useAdmin = () => {
+  const { user, loading } = useAuth();
+  const axiosSecure = useSecureAxios();
+
+  const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    queryKey: [user?.email, "isAdmin"],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/admin/${user.email}`);
+      return res.data?.admin;
+    },
+  });
+  
+  return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
