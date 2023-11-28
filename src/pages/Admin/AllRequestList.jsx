ee95373f16db564462afe53e9775c25001163008
coder7475 <robiulhossain7475@gmail.com>
2023-11-28 21:43:20 +0600

    feat: add reject handler to reject button of all request page

--- src/pages/Admin/AllRequestList.jsx
+++ src/pages/Admin/AllRequestList.jsx
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import useSecureAxios from "../../hooks/useSecureAxios";
 import useAdmin from "../../hooks/useAdmin";
+import Swal from "sweetalert2";
 
 const AllRequestList = () => {
   const axiosSecure = useSecureAxios();
   const [adminData, isAdminLoading] = useAdmin();
   const company = adminData?.user?.company;
   // console.log(company);
-  const { data: allRequests = [], isPending: isRequestLoading } = useQuery({
+  const { data: allRequests = [], isPending: isRequestLoading, refetch } = useQuery({
     queryKey: ["allRequests", company],
     enabled: !isAdminLoading,
     queryFn: async () => {
@@ -24,18 +25,35 @@ const AllRequestList = () => {
 
   const handleApproveRequest = (request) => {
     // console.log(request);
-    axiosSecure.put(`/admin/approveRequest/${request.name}`)
-      .then(res => {
-        console.log(res);
-      })
-  }
+    axiosSecure.put(`/admin/approveRequest/${request.name}`).then(() => {
+      // console.log(res);
+      refetch();
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Successfully Processed the Request!",
+      });
+    });
+  };
+
+  const handleRejectRequest = (request) => {
+    axiosSecure.put(`/admin/rejectRequest/${request.name}`).then((res) => {
+      console.log(res);
+      refetch();
+      Swal.fire({
+        icon: "error",
+        title: "Rejected",
+        text: "Successfully Rejected the Request!",
+      });
+    });
+  };
 
   return (
     <div className="mt-4 flex flex-col gap-5 items-center">
       <h1 className="font-bold text-center text-xl">All Request List</h1>
       <div className="grid grid-cols-1 gap-2 mt-5">
         {allRequests.map((empl) => (
-          <div          
+          <div
             key={empl._id}
             className="border-2 p-2 rounded-lg text-center space-y-2 bg-gray-200"
           >
@@ -76,11 +94,13 @@ const AllRequestList = () => {
                 onClick={() => handleApproveRequest(empl)}
                 type="button"
                 className="block w-32 hover:bg-black bg-blue-500 text-white p-2 rounded-xl"
-                disabled={ empl.status !== "pending"}
+                disabled={empl.status !== "pending"}
               >
                 Approve
               </button>
               <button
+                onClick={() => handleRejectRequest(empl)}
+                disabled={empl.status !== "pending"}
                 type="button"
                 className="block w-32 bg-blue-300 hover:bg-black text-white p-2 rounded-xl"
               >
