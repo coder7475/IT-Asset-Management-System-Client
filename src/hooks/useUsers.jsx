b634addd0330097311d2041ec24ab0e6073580ba
coder7475 <robiulhossain7475@gmail.com>
2023-11-28 09:36:49 +0600

    feat: add use Users hook to retrieve all users

---
+++ src/hooks/useUsers.jsx
@@ -0,0 +1,19 @@
+
+import useSecureAxios from './useSecureAxios';
+import { useQuery } from '@tanstack/react-query';
+
+const useEmployee = () => {
+  const axiosSecure = useSecureAxios();
+
+  const { data: allUsers = [], isPending: isUsersLoading } = useQuery({
+    queryKey: ["allUsers"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users`);
+      return res.data;
+    },
+  });
+
+  return [allUsers, isUsersLoading];
+};
+
+export default useEmployee;
