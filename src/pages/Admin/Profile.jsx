dac089c81939aa388f789e9e4da7c817ec9638c6
coder7475 <robiulhossain7475@gmail.com>
2023-11-29 15:24:08 +0600

    feat: show profile values as inpul with email as readonly

--- src/pages/Admin/Profile.jsx
+++ src/pages/Admin/Profile.jsx
@@ -2,17 +2,24 @@ import useAdmin from '../../hooks/useAdmin';
 
 const Profile = () => {
   const [adminData] = useAdmin();
-
   console.log(adminData.user);
+
+  const handleProfileUpdate = () => {
+    console.log("clicked update");
+  }
+
   return (
-    <div className="flex flex-col justify-start text-xl items-center bg-slate-400 min-h-screen">
+    <div className="flex flex-col justify-start text-xl items-center  min-h-screen">
       <h1 className="text-3xl font-bold mt-4">Profile Page</h1>  
-      <div className="mt-5 flex flex-col gap-3 justify-between items-start p-4  w-96 rounded-lg text-white">
-        <h1> <span className="font-semibold">Full Name:</span> {adminData.user.name}</h1>
-        <h1> <span className="font-semibold">Email:</span> {adminData.user.email}</h1>
-        <h1> <span className="font-semibold">Date Of Birth:</span> {adminData.user.birthday}</h1>
-        <button type="button" className="bg-blue-500 p-2 rounded-lg">Update</button>
-      </div> 
+      <form className="mt-5 flex flex-col gap-3 justify-center items-start p-4  w-96 rounded-lg border-2">
+        <span className="font-semibold">Full Name:</span>
+        <input type="text" defaultValue={adminData.user.name}/>
+        <span className="font-semibold">Email:</span>
+        <input type="text" defaultValue={adminData.user.email} readOnly/>
+        <span className="font-semibold">Date Of Birth:</span>
+        <input type="date" defaultValue={adminData.user.birthday} />
+        <button onClick={handleProfileUpdate} type="button" className="bg-blue-500 mx-auto p-2 rounded-lg text-white" >Update</button>
+      </form> 
     </div>
   );
 };
