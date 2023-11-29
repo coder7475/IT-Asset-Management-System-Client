72eefc8292d9c1ffd542f15928837166ac34f817
coder7475 <robiulhossain7475@gmail.com>
2023-11-29 15:28:48 +0600

    feat: add a event handler to profile page to update the profile data

--- src/pages/Admin/Profile.jsx
+++ src/pages/Admin/Profile.jsx
@@ -4,21 +4,31 @@ const Profile = () => {
   const [adminData] = useAdmin();
   console.log(adminData.user);
 
-  const handleProfileUpdate = () => {
-    console.log("clicked update");
+  const handleProfileUpdate = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const name = form.get("name");
+    const email = form.get("email");
+    const date = form.get("date");
+    const updatedDoc = {
+      name,
+      email,
+      date
+    }
+    console.log(updatedDoc);
   }
 
   return (
     <div className="flex flex-col justify-start text-xl items-center  min-h-screen">
       <h1 className="text-3xl font-bold mt-4">Profile Page</h1>  
-      <form className="mt-5 flex flex-col gap-3 justify-center items-start p-4  w-96 rounded-lg border-2">
-        <span className="font-semibold">Full Name:</span>
-        <input type="text" defaultValue={adminData.user.name}/>
+      <form className="mt-5 flex flex-col gap-3 justify-center items-start p-4  w-96 rounded-lg border-2" onSubmit={handleProfileUpdate}>
+        <span  className="font-semibold">Full Name:</span>
+        <input type="text"  name="name" defaultValue={adminData.user.name}/>
         <span className="font-semibold">Email:</span>
-        <input type="text" defaultValue={adminData.user.email} readOnly/>
+        <input type="text" name="email" defaultValue={adminData.user.email} readOnly/>
         <span className="font-semibold">Date Of Birth:</span>
-        <input type="date" defaultValue={adminData.user.birthday} />
-        <button onClick={handleProfileUpdate} type="button" className="bg-blue-500 mx-auto p-2 rounded-lg text-white" >Update</button>
+        <input type="date" name="date" defaultValue={adminData.user.birthday} />
+        <button type="submit" className="bg-blue-500 mx-auto p-2 rounded-lg text-white" >Update</button>
       </form> 
     </div>
   );
