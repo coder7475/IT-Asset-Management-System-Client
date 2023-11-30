0c340eda87be6f3d61f6d54308c7752b954fad8b
coder7475 <robiulhossain7475@gmail.com>
2023-11-30 09:24:39 +0600

    feat: implement axios inteceptor to catch invalid token

--- src/hooks/useSecureAxios.jsx
+++ src/hooks/useSecureAxios.jsx
@@ -1,4 +1,10 @@
 import axios from 'axios'
+import Swal from "sweetalert2";
+import auth from './../features/Authentication/firebase/firebase.config.js';
+import {
+  signOut
+} from "firebase/auth";
+import { redirect } from "react-router-dom";
 
 const instance = axios.create({
   baseURL: 'http://localhost:5000/api/v1',
@@ -6,6 +12,30 @@ const instance = axios.create({
 });
 
 const useSecureAxios = () => {
+  const logOut = () => {
+    return signOut(auth)
+      .then(() => {
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "You token is invalid!",
+      })});
+  };
+
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (error) => {
+      console.log('error tracked in the interceptor', error.response)
+      if (error.response.status === 401 || error.response.status === 403)
+      {      
+        logOut();
+        return redirect("/login");
+      }
+    }
+  );
+
   return instance;
 };
 
