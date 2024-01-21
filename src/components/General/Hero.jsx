68059af56a9c2a3c4d1452ff97198c54660bce50
coder7475 <robiulhossain7475@gmail.com>
2024-01-21 19:25:42 +0600

    fix: responsiveness issues of hero section

--- src/components/General/Hero.jsx
+++ src/components/General/Hero.jsx
@@ -4,12 +4,12 @@ import { NavLink } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="hero md:min-h-screen relative">
-      <div className="hero-bg md:absolute top-40 left-20  w-full md:block  md:w-[50%] lg:w-[40%] rounded p-5 space-y-3">
-        <h1 className="md:text-5xl font-bold">AssetIT</h1>
-        <h2 className="text-xl md:text-3xl font-medium">
+      <div className="hero-bg md:absolute top-10 xl:top-20 left-20  w-full md:block  md:w-[50%] lg:w-[40%] rounded p-5 space-y-3">
+        <h1 className="md:text-3xl font-bold">AssetIT</h1>
+        <h2 className="text-xl md:text-xl font-medium">
           Powerful and Intuitive IT Asset Management Software
         </h2>
-        <p className="para md:text-xl">
+        <p className="para md:text-lg">
           Maintain, track, and manage a single source of truth for your IT asset
           landscape. Always be audit-ready with easy-to-use IT asset management
           software.
